cee961a2fead1723bf6a1e65ee1bc7cff43e66c2
ajcool <jens.auerswald@gmail.com>
2019-04-09 09:37:22 +0200

    Changed export to es6

--- index.js
+++ index.js
@@ -2,6 +2,4 @@
 import IType from './src/IType'
 import { Type, Req } from './src/Types'
 
-module.exports.Type = Type
-module.exports.Req = Req
-module.exports.IType = IType
+export { Type, Req, IType }
